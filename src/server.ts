ff2f11cff45a61f0a8d64b08864753e02c29c27a
Neeraj Kashyap <nkashy1@gmail.com>
2023-07-08 07:33:02 -0700

    This is going to be a very simple server. Flattened structure
    No need for `routes/`.

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 // Configures the Umpire express server and starts the server process
 
 import cors from 'cors';
-import express from 'express';
+import express, {Request, Response} from 'express';
 
-import { PORT } from './config';
-import status from './routes/status';
+import { PORT, LEADERBOARD_ID } from './config';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/status', status);
+app.get('/status', async (_: Request, res: Response) => {
+  const responseBody = {LEADERBOARD_ID: LEADERBOARD_ID || ""}
+  return res.status(200).json(responseBody);
+});
 
 app.listen(PORT, () => { console.log(`Umpire Leaderboard Validation Service running on ${PORT}`) });
 
